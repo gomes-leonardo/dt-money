cd31400028a0c9587a89de571895f69be70c2695
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-02-15 19:37:08 -0300

    style: header and summary component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
+
+export default Header
