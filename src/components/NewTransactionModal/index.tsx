39ba507cf4f7ee933c453b3a640da9c9d4a20cc7
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-02-16 18:15:27 -0300

    style: css modal and framer motion transition

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,22 +1,33 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Content, Overlay } from './styles'
+import { motion } from 'framer-motion'
+import { CloseButton, Content, Overlay } from './styles'
+import { X } from 'phosphor-react'
 
 const NewTransactionModal = () => {
   return (
     <Dialog.Portal>
       <Overlay>
-        <Content>
-          <Dialog.Title>Nova transação</Dialog.Title>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Content>
+            <Dialog.Title>Nova transação</Dialog.Title>
+            <CloseButton>
+              <X size={20} />
+            </CloseButton>
 
-          <form action="">
-            <input type="text" placeholder="Descrição" required />
-            <input type="number" placeholder="Preço" required />
-            <input type="text" placeholder="Categoria" required />
+            <form action="">
+              <input type="text" placeholder="Descrição" required />
+              <input type="number" placeholder="Preço" required />
+              <input type="text" placeholder="Categoria" required />
 
-            <button type="submit">Cadastrar</button>
-          </form>
-          <Dialog.Close>X</Dialog.Close>
-        </Content>
+              <button type="submit">Cadastrar</button>
+            </form>
+          </Content>
+        </motion.div>
       </Overlay>
     </Dialog.Portal>
   )
